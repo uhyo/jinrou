5c83ce70940333f19d34064491d522f0fcd98a8d
uhyo <uhyohyo@uhyohyohyo.sakura.ne.jp>
2011-12-23 14:54:19 +0900

    data repair for blind

---
+++ tool/repair.js
@@ -0,0 +1,38 @@
+/*
+JINROU data recoverer:
+adds 'realid'!
+
+Contains a great deal of hard-coding...
+
+*/
+var user="test", password="test";	//自分でパスワードを入れてね
+
+var mongo=require('mongodb');
+var db=new mongo.Db("werewolf",new mongo.Server("localhost",27017));
+
+db.open(function(err,client){
+	if(err)console.log(err);
+	db.authenticate(user,password,function(err){
+		if(err)console.log(err);
+		db.collection("games",function(err,col){
+			if(err)console.log(err);
+			getdb(col);
+		});
+	});
+});
+
+function getdb(games){
+	games.find({}).toArray(function(err,docs){
+		if(err)console.log(err);
+		docs.forEach(function(game){
+			console.log(" - "+game.id);
+			game.players.forEach(function(pl){
+				console.log(pl.id+":"+pl.realid);
+				if(!pl.realid){
+					pl.realid=pl.id;
+				}
+			});
+			games.update({id:game.id},game);
+		});
+	});
+}
