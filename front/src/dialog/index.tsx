946eb87bbbd57086766197f6cdfcc18476f89386
uhyo <uhyo@uhy.ooo>
2018-02-15 01:54:40 +0900

    revival of confirmDialog

--- front/src/dialog/index.tsx
+++ front/src/dialog/index.tsx
@@ -39,6 +39,8 @@ export function showConfirmDialog(d: IConfirmDialog): Promise<boolean> {
                 {...d}
                 onSelect={close}
             />);
+
+            open(dialog);
         });
 }
 
