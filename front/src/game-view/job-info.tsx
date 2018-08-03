33d8cdbc4fc9c598f5abfcfea7d75cff9d7d9f79
uhyo <uhyo@uhy.ooo>
2018-08-04 01:25:10 +0900

    Move remaining text to language file

--- front/src/game-view/job-info.tsx
+++ front/src/game-view/job-info.tsx
@@ -4,7 +4,7 @@ import { WithSeparator } from '../util/with-separator';
 
 import { RoleInfo, RolePeersInfo, RoleOtherPlayerInfo } from './defs';
 
-import { I18nInterp } from '../i18n';
+import { I18nInterp, I18n } from '../i18n';
 
 const Wrapper = styled.div`
   margin: 5px;
@@ -51,117 +51,125 @@ export class JobInfo extends React.PureComponent<IPropJobInfo, {}> {
       supporting,
     } = this.props;
     return (
-      <Wrapper>
-        {/* Job Status. */}
-        <p>
-          <I18nInterp ns="game_client" k="jobinfo.status">
-            {{
-              job: <b>{jobname}</b>,
-              details:
-                desc.length === 0
-                  ? null
-                  : [
-                      ...mapJoin(desc, '・', (obj, idx) => (
-                        <React.Fragment key={`${idx}-${obj.type}`}>
-                          <a
-                            href={`/manual/job/${
-                              obj.type
-                            }?jobname=${encodeURIComponent(obj.name)}`}
-                            data-jobname={obj.name}
-                          >
-                            {desc.length === 1 ? '詳細' : `${obj.name}の詳細`}
-                          </a>
-                        </React.Fragment>
-                      )),
-                    ],
-            }}
-          </I18nInterp>
-        </p>
-        {/* Team info when provided. */}
-        {myteam != null ? (
-          <p>
-            {myteam === '' ? (
-              <I18nInterp ns="game_client" k="jobinfo.team.none" />
-            ) : (
-              <I18nInterp ns="game_client" k="jobinfo.team.message">
+      <I18n namespace="game_client">
+        {t => (
+          <Wrapper>
+            {/* Job Status. */}
+            <p>
+              <I18nInterp ns="game_client" k="jobinfo.status">
                 {{
-                  team: (
-                    <b>
-                      <I18nInterp ns="roles" k={`teamName.${myteam}`} />
-                    </b>
-                  ),
+                  job: <b>{jobname}</b>,
+                  details:
+                    desc.length === 0
+                      ? null
+                      : [
+                          ...mapJoin(desc, '・', (obj, idx) => (
+                            <React.Fragment key={`${idx}-${obj.type}`}>
+                              <a
+                                href={`/manual/job/${
+                                  obj.type
+                                }?jobname=${encodeURIComponent(obj.name)}`}
+                                data-jobname={obj.name}
+                              >
+                                {desc.length === 1
+                                  ? t('jobinfo.detail_one')
+                                  : t('jobinfo.detail', { job: obj.name })}
+                              </a>
+                            </React.Fragment>
+                          )),
+                        ],
                 }}
               </I18nInterp>
+            </p>
+            {/* Team info when provided. */}
+            {myteam != null ? (
+              <p>
+                {myteam === '' ? (
+                  t('jobinfo.team.none')
+                ) : (
+                  <I18nInterp ns="game_client" k="jobinfo.team.message">
+                    {{
+                      team: (
+                        <b>
+                          <I18nInterp ns="roles" k={`teamName.${myteam}`} />
+                        </b>
+                      ),
+                    }}
+                  </I18nInterp>
+                )}
+              </p>
+            ) : null}
+            {/* Info of peers. */}
+            {peers.map(key => {
+              const pls = this.props[key];
+              if (pls == null || pls.length === 0) {
+                return null;
+              }
+              // If this field exists, list up name of players.
+              const names = pls.map(({ id, name }) => <b key={id}>{name}</b>);
+              return (
+                <p key={`peers-${key}`}>
+                  <I18nInterp ns="game_client" k={`jobinfo.peers.${key}`}>
+                    {{
+                      names: (
+                        <WithSeparator separator="，">{names}</WithSeparator>
+                      ),
+                    }}
+                  </I18nInterp>
+                </p>
+              );
+            })}
+            {otherPlayerKeys.map(key => {
+              const pl = this.props[key];
+              if (pl == null) {
+                return;
+              }
+              return (
+                <p key={`otherPlayer-${key}`}>
+                  <I18nInterp ns="game_client" k={`jobinfo.peers.${key}`}>
+                    {{
+                      name: <b>{pl.name}</b>,
+                    }}
+                  </I18nInterp>
+                </p>
+              );
+            })}
+            {/* Handling of special peer info. */}
+            {quantumwerewolf_number == null ? null : (
+              <p>
+                <I18nInterp
+                  ns="game_client"
+                  k="jobinfo.peers.quantumwerewolfNumber"
+                >
+                  {{
+                    number: quantumwerewolf_number,
+                  }}
+                </I18nInterp>
+              </p>
             )}
-          </p>
-        ) : null}
-        {/* Info of peers. */}
-        {peers.map(key => {
-          const pls = this.props[key];
-          if (pls == null || pls.length === 0) {
-            return null;
-          }
-          // If this field exists, list up name of players.
-          const names = pls.map(({ id, name }) => <b key={id}>{name}</b>);
-          return (
-            <p key={`peers-${key}`}>
-              <I18nInterp ns="game_client" k={`jobinfo.peers.${key}`}>
-                {{
-                  names: <WithSeparator separator="，">{names}</WithSeparator>,
-                }}
-              </I18nInterp>
-            </p>
-          );
-        })}
-        {otherPlayerKeys.map(key => {
-          const pl = this.props[key];
-          if (pl == null) {
-            return;
-          }
-          return (
-            <p key={`otherPlayer-${key}`}>
-              <I18nInterp ns="game_client" k={`jobinfo.peers.${key}`}>
-                {{
-                  name: <b>{pl.name}</b>,
-                }}
-              </I18nInterp>
-            </p>
-          );
-        })}
-        {/* Handling of special peer info. */}
-        {quantumwerewolf_number == null ? null : (
-          <p>
-            <I18nInterp
-              ns="game_client"
-              k="jobinfo.peers.quantumwerewolfNumber"
-            >
-              {{
-                number: quantumwerewolf_number,
-              }}
-            </I18nInterp>
-          </p>
+            {supporting == null ? null : (
+              <p>
+                <I18nInterp ns="game_client" k="jobinfo.peers.supprting">
+                  {{
+                    name: <b>{supporting.name}</b>,
+                    job: <b>{supporting.supportingJob}</b>,
+                  }}
+                </I18nInterp>
+              </p>
+            )}
+            {/* Victory or defeat. */}
+            {win === true ? (
+              <p>
+                <I18nInterp ns="game_client" k="jobinfo.win" />
+              </p>
+            ) : win === false ? (
+              <p>
+                <I18nInterp ns="game_client" k="jobinfo.lose" />
+              </p>
+            ) : null}
+          </Wrapper>
         )}
-        {supporting == null ? null : (
-          <p>
-            <I18nInterp ns="game_client" k="jobinfo.peers.supprting">
-              {{
-                name: <b>{supporting.name}</b>,
-                job: <b>{supporting.supportingJob}</b>,
-              }}
-            </I18nInterp>
-          </p>
-        )}
-        {/* Victory or defeat. */}
-        {win === true ? (
-          <p>
-            <I18nInterp ns="game_client" k="jobinfo.win" />
-          </p>
-        ) : win === false ? (
-          <p>
-            <I18nInterp ns="game_client" k="jobinfo.lose" />
-          </p>
-        ) : null}
-      </Wrapper>
+      </I18n>
     );
   }
 }
