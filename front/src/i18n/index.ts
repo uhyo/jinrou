38af6db1bf580e1fc0f8667ffca381a9eed0952f
uhyo <uhyo@uhy.ooo>
2018-09-20 01:17:46 +0900

    do not escape in i18n
    close #277

--- front/src/i18n/index.ts
+++ front/src/i18n/index.ts
@@ -9,6 +9,7 @@ i18next.use(xhrBackend).init({
     parse: (data: any) => data,
     ajax,
   },
+  escapeValue: false,
   fallbackLng: EXTERNAL_SYSTEM_LANGUAGE,
   ns: [
     'common',
@@ -18,7 +19,7 @@ i18next.use(xhrBackend).init({
     'rules',
     'casting',
   ],
-});
+} as any);
 
 /**
  * Reexport type of i18n.
