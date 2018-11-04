d36c6504a6cba18042279693475efe91ece6850b
uhyo <uhyo@uhy.ooo>
2018-11-05 01:20:17 +0900

    add IsPhone helper component

---
+++ front/src/common/media.tsx
@@ -0,0 +1,79 @@
+import { css } from '../util/styled';
+import { SimpleInterpolation } from 'styled-components';
+import * as React from 'react';
+
+// media queries
+
+/**
+ * Width of viewport to switch phone mode and pc mode.
+ */
+const phoneWidth = '600px';
+
+/**
+ * Query string to switch to phone mode.
+ */
+const phoneQuery = `max-width: ${phoneWidth}`;
+
+/**
+ * Media query for smartphones.
+ */
+export const phone = (
+  ...args: [TemplateStringsArray, ...SimpleInterpolation[]]
+) => css`
+  @media (${phoneQuery}) {
+    ${css(...args)};
+  }
+`;
+
+/**
+ * Media query for non-smartphones.
+ */
+export const notPhone = (
+  ...args: [TemplateStringsArray, ...SimpleInterpolation[]]
+) => css`
+  @media (not(${phoneQuery})) {
+    ${css(...args)};
+  }
+`;
+
+/**
+ * Component which watches query.
+ */
+export class IsPhone extends React.Component<
+  {
+    children: (isPhone: boolean) => React.ReactNode;
+  },
+  {
+    isPhone: boolean;
+  }
+> {
+  public state = { isPhone: false };
+  private mediaQueryList = window.matchMedia(`(${phoneQuery})`);
+  private eventHandler: (() => void) | null = null;
+  public render() {
+    const {
+      props: { children },
+      state: { isPhone },
+    } = this;
+
+    return children(isPhone);
+  }
+  public componentDidMount() {
+    const { mediaQueryList } = this;
+    this.eventHandler = () => {
+      this.setState({
+        isPhone: mediaQueryList.matches,
+      });
+    };
+    // reflect current matching state of media query.
+    this.eventHandler();
+
+    // mediaQueryList.addEventListener is not Typable yet! (TS 3.0.3)
+    mediaQueryList.addListener(this.eventHandler);
+  }
+  public componentWillUnmount() {
+    if (this.eventHandler != null) {
+      this.mediaQueryList.removeListener(this.eventHandler);
+    }
+  }
+}
