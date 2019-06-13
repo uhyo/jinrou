93ef0861424998e9b041e854e00c071244f330fa
uhyo <uhyo@uhy.ooo>
2019-06-13 14:32:05 +0900

    extract 5px in forms

--- front/src/common/style.ts
+++ front/src/common/style.ts
@@ -7,3 +7,8 @@ export const smallTextSize = '0.9em';
  * padding of whole-page app.
  */
 export const pagePadding = '20px';
+
+/**
+ * Standard vertical margin between form components.
+ */
+export const formComponentsVerticalMergin = '5px';
