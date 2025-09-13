7fece07e71a86d003f08b5fe3270f70fbd93201b
Nanaya716 <130658278+Nanaya716@users.noreply.github.com>
2025-09-14 05:47:08 +0800

    Fix the issue where filtering comments still triggers reflow, leading to performance problems. TODO:it still appears when clicking 'Rules.' (#213)
    Co-authored-by: Nanaya716 <714991699@qq.com>

--- front/src/pages/game-view/logs/elements.tsx
+++ front/src/pages/game-view/logs/elements.tsx
@@ -24,6 +24,7 @@ export const LogWrapper = styled.div<{
   fixedSize: boolean;
 }>`
   width: 100%;
+  contain: layout style;
   display: ${props => (props.fixedSize ? 'block' : 'grid')};
   grid-template-columns:
     minmax(8px, max-content)
